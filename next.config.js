19685949a0a3e682b3ff6a72ea3de8e22ffdbfca
MediaZ DEV <116047433+trungducnet123@users.noreply.github.com>
2023-10-25 11:50:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  basePath: '/'
-}
-
-module.exports = nextConfig
+module.exports = {
+    basePath: '/random-tarot',
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/random-tarot',
+                basePath: false,
+                permanent: false
+            }
+        ]
+    }
+};
